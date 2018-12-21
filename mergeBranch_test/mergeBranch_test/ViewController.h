5c202380ff8c506119c1405bd3b8063fe3a45d9a
test <1436120982@qq.com>
2018-12-21 17:34:25 +0800

    项目初始化

---
+++ mergeBranch_test/mergeBranch_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mergeBranch_test
+//
+//  Created by licy on 2018/12/21.
+//  Copyright © 2018 suning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mergeBranch_test/mergeBranch_test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mergeBranch_test
+//
+//  Created by licy on 2018/12/21.
+//  Copyright © 2018 suning. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel * labelA = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 50, 50)];
+    labelA.text = @"labelA";
+    [self.view addSubview:labelA];
+}
+
+
+@end

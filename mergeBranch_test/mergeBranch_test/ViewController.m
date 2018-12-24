1b03dfce515326dc8af57e5b12f446047b3ada16
licunyao <18070521@cnsuning.com>
2018-12-24 10:30:36 +0800

    1111

--- mergeBranch_test/mergeBranch_test/ViewController.m
+++ mergeBranch_test/mergeBranch_test/ViewController.m
@@ -17,14 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    UILabel * BBB = [[UILabel alloc] initWithFrame:CGRectMake(100, 300, 50, 50)];
-    BBB.text = @"BBB";
-    [self.view addSubview:BBB];
-
-    UILabel * AAA1 = [[UILabel alloc] initWithFrame:CGRectMake(100, 200, 50, 50)];
-    AAA1.text = @"AAA1";
-    [self.view addSubview:AAA1];
 }
 
 
